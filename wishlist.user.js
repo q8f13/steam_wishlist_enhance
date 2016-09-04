e2d017eb012c1aa5939afa368aabd99d14b0fc9e
q8f13 <dustfall@gmail.com>
2016-09-04 11:53:18 +0800

    save issue fix

--- wishlist.user.js
+++ wishlist.user.js
@@ -4,25 +4,22 @@
 // @include     http://steamcommunity.com/id/*/wishlist
 // @exclude
 // @version     1
+// @description	find out games in your wishlist which are on sale and pin them top with highlight
 // @grant       none
 // @require  https://code.jquery.com/jquery-3.1.0.min.js
 // ==/UserScript==
-// a function that loads jQuery and calls a callback function when jQuery has finished loading
 function main() {
 	$(document).ready(moveDiscountItemUp);
 }
 function moveDiscountItemUp() {
-	// console.log("discount counts: " + $('div.discount_prices').length);
-	// console.log("discount parent counts: " + $('div.discount_prices').closest('div.wishlistRow').length);
 	// get the first game in list
 	var first = $('div.wishlistRow').first();
 	// unshift the discount one
 	var discountList = $('div.discount_prices').closest('div.wishlistRow');
 	first.before(discountList);
-	discountList.css({
-		background-color: #00ff00,
-		border: 5px solid #33ff33
-	});
+	// highlight them
+	discountList.css('border', '3px solid #4c6b22');
+	);
 }
 
 main();
